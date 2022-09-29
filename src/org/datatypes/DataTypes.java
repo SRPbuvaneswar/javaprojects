e0abfb29cfe70b3eca5b11190a52e996e7adc0d8
SRPbuvaneswar <bhuvanesh.csc@gmail.com>
2022-09-29 15:13:53 +0530

    file created by buvaneswar

---
+++ src/org/datatypes/DataTypes.java
@@ -0,0 +1,35 @@
+package org.datatypes;
+
+public class DataTypes {
+	
+	private void employeeDetail() {
+		byte empId = 100;
+	System.out.println(empId);
+	
+	 String empName = "S R BUVANESWAR";
+	  System.out.println(empName);
+	
+	  String empMail ="bhuvanesh.csc@gmail.com";
+	  System.out.println(empMail);
+	  
+	 long empPhone = 8056506367L;
+	  System.out.println(empPhone);
+	  
+	  double Salary = 118000.852333;
+	  System.out.println(Salary);
+	  
+	  char Gender = 'M';
+	  System.out.println(Gender);
+	  
+	  String City = "Tiruppur";
+	  System.out.println(City);
+
+	}
+	public static void main(String[] args) {
+		DataTypes d = new DataTypes();
+		d.employeeDetail();
+		
+		
+	}
+
+}
